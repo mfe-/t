b575c570235ce401450555e29fbfbfe5fa0bb923
mfe <martin.fetzel@get-the-solution.net>
2021-09-26 12:26:27 +0200

    a exception while connecting should not treated in a way that the next method PlayGame will be executed

--- lib/GameSocketClient.cs
+++ lib/GameSocketClient.cs
@@ -51,74 +51,65 @@ namespace t.lib
             byte[] bytes = new byte[1024];
             _player = new Player(name, _guid);
             // Connect to a remote device.  
+
+            // Establish the remote endpoint for the socket.  
+            // This example uses port 11000 on the local computer.  
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            //IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(serverIpAdress, serverPort);
+
+            // Create a TCP/IP  socket.  
+            _senderSocket = new Socket(serverIpAdress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            if (SenderSocket == null) throw new InvalidOperationException($"{nameof(SenderSocket)} should not be null!");
+            // Connect the socket to the remote endpoint. Catch any errors.  
             try
             {
-                // Establish the remote endpoint for the socket.  
-                // This example uses port 11000 on the local computer.  
-                IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
-                //IPAddress ipAddress = ipHostInfo.AddressList[0];
-                IPEndPoint remoteEP = new IPEndPoint(serverIpAdress, serverPort);
+                SenderSocket.Connect(remoteEP);
 
-                // Create a TCP/IP  socket.  
-                _senderSocket = new Socket(serverIpAdress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-                if (SenderSocket == null) throw new InvalidOperationException($"{nameof(SenderSocket)} should not be null!");
-                // Connect the socket to the remote endpoint. Catch any errors.  
-                try
+                _logger.LogInformation("Socket connected to {0}", SenderSocket.RemoteEndPoint?.ToString() ?? $"Could not determine {nameof(_senderSocket.RemoteEndPoint)}");
+
+                GameActionProtocol gameActionProtocol = GameActionProtocolFactory(Constants.RegisterPlayer, _player);
+                // Encode the data string into a byte array.  
+                byte[] sendPayLoad = gameActionProtocol.ToByteArray();
+                // Send the data through the socket.  
+                _logger.LogInformation("PlayerId {gamePlayerId} for {playerName} generated", gameActionProtocol.PlayerId, GetPlayer(gameActionProtocol).Name);
+                int bytesSent = await SenderSocket.SendAsync(new ArraySegment<byte>(sendPayLoad), SocketFlags.None);
+                _logger.LogTrace("Sent {0} bytes to server.", bytesSent);
+                _logger.LogInformation("Waiting for remaining players to join");
+                //wait until all players joined
+                GameActionProtocol gameActionProtocolRec = new GameActionProtocol();
+                gameActionProtocolRec.Phase = Constants.Ok;
+                while (gameActionProtocolRec.Phase != Constants.StartGame)
                 {
-                    SenderSocket.Connect(remoteEP);
-
-                    _logger.LogInformation("Socket connected to {0}", SenderSocket.RemoteEndPoint?.ToString() ?? $"Could not determine {nameof(_senderSocket.RemoteEndPoint)}");
-
-                    GameActionProtocol gameActionProtocol = GameActionProtocolFactory(Constants.RegisterPlayer, _player);
-                    // Encode the data string into a byte array.  
-                    byte[] sendPayLoad = gameActionProtocol.ToByteArray();
-                    // Send the data through the socket.  
-                    _logger.LogInformation("PlayerId {gamePlayerId} for {playerName} generated", gameActionProtocol.PlayerId, GetPlayer(gameActionProtocol).Name);
-                    int bytesSent = await SenderSocket.SendAsync(new ArraySegment<byte>(sendPayLoad), SocketFlags.None);
+                    // Receive the response from the remote device.  
+                    bytes = new byte[1024];
+                    int bytesRec = SenderSocket.Receive(bytes);
+                    _logger.LogTrace("Received {0} bytes.", bytesRec);
+                    gameActionProtocolRec = bytes.AsSpan().Slice(0, bytesRec).ToArray().ToGameActionProtocol(bytesRec);
+                    await OnMessageReceiveAsync(gameActionProtocolRec);
+                    //send ok
+                    sendPayLoad = GameActionProtocolFactory(Constants.Ok).ToByteArray();
+                    bytesSent = await SenderSocket.SendAsync(new ArraySegment<byte>(sendPayLoad), SocketFlags.None);
                     _logger.LogTrace("Sent {0} bytes to server.", bytesSent);
-                    _logger.LogInformation("Waiting for remaining players to join");
-                    //wait until all players joined
-                    GameActionProtocol gameActionProtocolRec = new GameActionProtocol();
-                    gameActionProtocolRec.Phase = Constants.Ok;
-                    while (gameActionProtocolRec.Phase != Constants.StartGame)
-                    {
-                        // Receive the response from the remote device.  
-                        bytes = new byte[1024];
-                        int bytesRec = SenderSocket.Receive(bytes);
-                        _logger.LogTrace("Received {0} bytes.", bytesRec);
-                        gameActionProtocolRec = bytes.AsSpan().Slice(0, bytesRec).ToArray().ToGameActionProtocol(bytesRec);
-                        await OnMessageReceiveAsync(gameActionProtocolRec);
-                        //send ok
-                        sendPayLoad = GameActionProtocolFactory(Constants.Ok).ToByteArray();
-                        bytesSent = await SenderSocket.SendAsync(new ArraySegment<byte>(sendPayLoad), SocketFlags.None);
-                        _logger.LogTrace("Sent {0} bytes to server.", bytesSent);
-                    }
-
-                }
-                catch (ArgumentNullException ane)
-                {
-                    _logger.LogCritical(ane, "ArgumentNullException : {0}", ane.ToString());
-                }
-                catch (SocketException se)
-                {
-                    if (se.ErrorCode == 10054)
-                    {
-                        _logger.LogCritical(se, "Lost connection to {0} SocketException : {1}", serverIpAdress, se.ToString());
-                    }
-                    else
-                    {
-                        _logger.LogCritical(se, "Could not connect to {0} SocketException : {1}", serverIpAdress, se.ToString());
-                    }
-                }
-                catch (Exception e)
-                {
-                    _logger.LogCritical(e, "Unexpected exception : {0}", e.ToString());
                 }
 
             }
-            catch (Exception e)
+            catch (ArgumentNullException ane)
             {
-                _logger.LogCritical(e, e.ToString());
+                _logger.LogCritical(ane, "ArgumentNullException : {0}", ane.ToString());
+                throw;
+            }
+            catch (SocketException se)
+            {
+                if (se.ErrorCode == 10054)
+                {
+                    _logger.LogCritical(se, "Lost connection to {0} SocketException : {1}", serverIpAdress, se.ToString());
+                }
+                else
+                {
+                    _logger.LogCritical(se, "Could not connect to {0} SocketException : {1}", serverIpAdress, se.ToString());
+                }
+                throw;
             }
         }
 

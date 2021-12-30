df4ec2ee0f3085fb1aec53e3136dbd5d7100fba1
MartinF <mfe@live.de>
2021-12-30 13:43:28 +0100

    maui project

---
+++ t.App/NewGame.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+using System;
+
+namespace t.App
+{
+    public partial class NewGame : ContentPage
+    {
+        public NewGame()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ t.App/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,12 @@
+﻿using Foundation;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+
+namespace t.App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

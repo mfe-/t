4acb78e98dbfc688a47c51b926e45d2b42635f49
mfe <martin.fetzel@get-the-solution.net>
2022-02-12 21:10:37 +0100

    add join game stuff

--- t.App/View/JoinGamePageViewModel.cs
+++ t.App/View/JoinGamePageViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using System.Windows.Input;
 
 namespace t.App.View;
 
@@ -6,6 +7,39 @@ internal class JoinGamePageViewModel : BaseViewModel
 {
     public JoinGamePageViewModel(ILogger<JoinGamePageViewModel> logger) : base(logger)
     {
+        JoinGameCommand = new Command(OnJoinGame);
     }
+
     public String Title { get; set; } = "Join a game";
+
+
+    private string _ServerIp="10.51.51.100";
+    public string ServerIp
+    {
+        get { return _ServerIp; }
+        set { SetProperty(ref _ServerIp, value, nameof(ServerIp)); }
+    }
+
+
+    private string _ServerPort="12000";
+    public string ServerPort
+    {
+        get { return _ServerPort; }
+        set { SetProperty(ref _ServerPort, value, nameof(ServerPort)); }
+    }
+
+
+    private string _PlayerName="";
+    public string PlayerName
+    {
+        get { return _PlayerName; }
+        set { SetProperty(ref _PlayerName, value, nameof(PlayerName)); }
+    }
+
+
+    public ICommand JoinGameCommand { get; }
+    private void OnJoinGame()
+    {
+        
+    }
 }

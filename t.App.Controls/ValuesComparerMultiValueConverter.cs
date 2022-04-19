6a0c5e5ab31c1257d64e89e45ee1b14d79563c2f
Martin Fetzel <martin.fetzel@get-the-solution.net>
2022-04-19 17:56:52 +0200

    Determine selected card with valuescomparermultivalueconverter

---
+++ t.App.Controls/ValuesComparerMultiValueConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t.App.Controls;
+
+public class ValuesComparerMultiValueConverter : IMultiValueConverter
+{
+    public static readonly string  TreatNullAsFalse = "TreatNullAsFalse";
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if(values.Length >= 2)
+        {
+            if((string)parameter == TreatNullAsFalse)
+            {
+                return values[0] != null && values[1] != null && values[0] == values[1];
+            }
+            return values[0] == values[1];
+        }
+        throw new InvalidOperationException($"{nameof(values)} array expects at least two elements for comparing!" );
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
--- t.App/View/DebugPage.xaml.cs
+++ t.App/View/DebugPage.xaml.cs
@@ -1,3 +1,5 @@
+using Microsoft.Maui.Controls;
+
 namespace t.App.View;
 
 public partial class DebugPage : ContentPage
@@ -5,5 +7,18 @@ public partial class DebugPage : ContentPage
 	public DebugPage()
 	{
 		InitializeComponent();
+        Loaded += DebugPage_Loaded;
 	}
+
+    private void DebugPage_Loaded(object? sender, EventArgs e)
+    {
+        var tap = new TapGestureRecognizer();
+        tap.Tapped += Tap_Tapped;
+        label1.GestureRecognizers.Add(tap);
+    }
+
+    private void Tap_Tapped(object? sender, EventArgs e)
+    {
+        
+    }
 }

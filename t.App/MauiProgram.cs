f98c869ac61725ee7baf5f382cd6464388e2619b
mfe <martin.fetzel@get-the-solution.net>
2022-05-13 18:59:37 +0200

    upgrade to next visual studio maui tool chain

--- t.App/MauiProgram.cs
+++ t.App/MauiProgram.cs
@@ -3,65 +3,64 @@ using t.App.Service;
 using t.App.View;
 using t.lib;
 
-namespace t.App
+namespace t.App;
+
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                //fonts.AddFont("Halogen-Bold.ttf", "HalogenBold");
+                fonts.AddEmbeddedResourceFont(typeof(Controls.CardView).Assembly, "Halogen-Bold.ttf", "HalogenBold");
+            });
+
+
+        builder.Services.AddSingleton(
+            provider =>
+            {
+                return new NavigationPage(provider.GetRequiredService<MainPage>())
                 {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    //fonts.AddFont("Halogen-Bold.ttf", "HalogenBold");
-                    fonts.AddEmbeddedResourceFont(typeof(Controls.CardView).Assembly, "Halogen-Bold.ttf", "HalogenBold");
-                });
+                    //BindingContext = provider.GetRequiredService<MainPageViewModel>()
+                };
+            });
 
+        AppConfig appConfig = new AppConfig();
+        appConfig.ServerPort = 12000;
+        appConfig.BroadcastPort = 15000;
 
-            builder.Services.AddSingleton(
-                provider =>
-                {
-                    return new NavigationPage(provider.GetRequiredService<MainPage>())
-                    {
-                        //BindingContext = provider.GetRequiredService<MainPageViewModel>()
-                    };
-                });
+        builder.Services.AddSingleton(appConfig);
 
-            AppConfig appConfig = new AppConfig();
-            appConfig.ServerPort = 12000;
-            appConfig.BroadcastPort = 15000;
+        builder.Services.AddTransient<MainPage>();
+        builder.Services.AddSingleton<MainPageViewModel>();
 
-            builder.Services.AddSingleton(appConfig);
+        builder.Services.AddTransient<GamePage>();
+        builder.Services.AddSingleton<GamePageViewModel>();
 
-            builder.Services.AddTransient<MainPage>();
-            builder.Services.AddSingleton<MainPageViewModel>();
+        builder.Services.AddTransient<NewGamePage>();
+        builder.Services.AddSingleton<NewGamePageViewModel>();
 
-            builder.Services.AddTransient<GamePage>();
-            builder.Services.AddSingleton<GamePageViewModel>();
+        builder.Services.AddTransient<JoinGamePage>();
+        builder.Services.AddSingleton<JoinGamePageViewModel>();
 
-            builder.Services.AddTransient<NewGamePage>();
-            builder.Services.AddSingleton<NewGamePageViewModel>();
+        builder.Services.AddTransient<SettingPage>();
+        builder.Services.AddSingleton<SettingPageViewModel>();
 
-            builder.Services.AddTransient<JoinGamePage>();
-            builder.Services.AddSingleton<JoinGamePageViewModel>();
+        builder.Services.AddTransient<DebugPage>();
+        builder.Services.AddTransient<DebugPageViewModel>();
 
-            builder.Services.AddTransient<SettingPage>();
-            builder.Services.AddSingleton<SettingPageViewModel>();
+        builder.Services.AddSingleton(
+            provider => new NavigationService(
+            provider.GetRequiredService<IServiceProvider>(),
+            provider.GetRequiredService<ILogger<NavigationService>>(),
+            provider.GetRequiredService<NavigationPage>()));
 
-            builder.Services.AddTransient<DebugPage>();
-            builder.Services.AddTransient<DebugPageViewModel>();
+        builder.Services.AddSingleton<GameService>();
 
-            builder.Services.AddSingleton(
-                provider => new NavigationService(
-                provider.GetRequiredService<IServiceProvider>(),
-                provider.GetRequiredService<ILogger<NavigationService>>(),
-                provider.GetRequiredService<NavigationPage>()));
-
-            builder.Services.AddSingleton<GameService>();
-
-            return builder.Build();
-        }
+        return builder.Build();
     }
 }

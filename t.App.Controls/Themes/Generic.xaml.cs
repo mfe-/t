242e9ecd051b6d74117d3878bac917761be39a15
MartinF <mfe@live.de>
2022-07-02 17:49:16 +0200

    stop searching for games after we are joining a game

--- t.App.Controls/Themes/Generic.xaml.cs
+++ t.App.Controls/Themes/Generic.xaml.cs
@@ -1,47 +1,48 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace t.App.Controls.Themes
-//{
-//    public partial class Generic : ResourceDictionary
-//    {
-//        private static bool registered;
+namespace t.App.Controls.Themes
+{
+    public partial class Generic : ResourceDictionary
+    {
+        private static bool registered;
+        public Generic()
+        {
+            InitializeComponent();
+            EnsureRegistered(this);
+        }
+        internal static void EnsureRegistered(ResourceDictionary resourceDictionary)
+        {
+            // don't do extra work
+            if (registered)
+                return;
 
-//        public Generic()
-//        {
-//            EnsureRegistered(this);
-//        }
+            // get the dictionary if we can
+            var merged = Application.Current?.Resources?.MergedDictionaries;
+            if (merged != null)
+            {
+                // check to see if we are added already
+                foreach (var dic in merged)
+                {
+                    if (dic.GetType() == typeof(Generic))
+                    {
+                        registered = true;
+                        break;
+                    }
+                }
 
-//        internal static void EnsureRegistered(ResourceDictionary resourceDictionary)
-//        {
-//            // don't do extra work
-//            if (registered)
-//                return;
+                // if we are not added, add ourselves
+                if (!registered)
+                {
+                    merged.Add(resourceDictionary);
+                    registered = true;
+                }
+            }
+        }
+    }
+}
 
-//            // get the dictionary if we can
-//            var merged = Application.Current?.Resources?.MergedDictionaries;
-//            if (merged != null)
-//            {
-//                // check to see if we are added already
-//                foreach (var dic in merged)
-//                {
-//                    if (dic.GetType() == typeof(Generic))
-//                    {
-//                        registered = true;
-//                        break;
-//                    }
-//                }
-
-//                // if we are not added, add ourselves
-//                if (!registered)
-//                {
-//                    merged.Add(resourceDictionary);
-//                    registered = true;
-//                }
-//            }
-//        }
-//    }
-//}
